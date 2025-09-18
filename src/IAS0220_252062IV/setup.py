fff3e17345f940f4911e565414cb64a23580873b
Bumbaluk <bumbaluk@cvut.cz>
2025-09-19 01:32:54 +0300

    Initial commit

---
+++ src/IAS0220_252062IV/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'IAS0220_252062IV'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']), 
+         # ✅ Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+
+        # ✅ Install URDF files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lukas bumba',
+    maintainer_email='lukabu@taltech.ee',
+    description='Package ready for task 2',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
